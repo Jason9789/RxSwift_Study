c76dff9f5efc87b0cab93e3f43bea8ad3491ecf2
Jason9789 <lifebook0809@gmail.com>
2021-03-29 20:22:33 +0900

    [INIT] Project init

---
+++ CameraFilter/CameraFilter/PhotoCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewController.swift
+//  CameraFilter
+//
+//  Created by 전판근 on 2021/03/29.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewcontroller: UICollectionViewController {
+    
+}
---
+++ CameraFilter/CameraFilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraFilter
+//
+//  Created by 전판근 on 2021/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+
+}
+
