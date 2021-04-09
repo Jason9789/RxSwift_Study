a3725140487ca258c3b07d78d418cb54401055ea
Jason9789 <lifebook0809@gmail.com>
2021-04-10 03:12:40 +0900

    feature: SnapKit 추가, Main View 설정

--- CameraFilter/CameraFilter/ViewController.swift
+++ CameraFilter/CameraFilter/ViewController.swift
@@ -7,16 +7,63 @@
 
 import UIKit
 import RxSwift
-
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    let imageView = UIImageView()
+    var applyFilterButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Apply Filter", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        btn.backgroundColor = .systemBlue
+        
+        return btn
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationInit()
+        addView()
+    }
+    
+    private func navigationInit() {
+        self.view.backgroundColor = .white
+        self.navigationItem.title = "Camera Filter"
         self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "add", style: .plain, target: self, action: #selector(addTapped))
+    }
+    
+    private func addView() {
+        self.view.addSubview(imageView)
+        self.view.addSubview(applyFilterButton)
+        
+        imageView.image = .add
+        imageView.snp.makeConstraints { (make) in
+            make.top.equalTo(self.view).offset(100)
+            make.left.right.equalTo(self.view)
+            make.bottom.equalTo(self.view).offset(-100)
+        }
+        
+        applyFilterButton.snp.makeConstraints { (make) in
+            make.top.equalTo(imageView.snp.bottom).offset(30)
+            make.centerX.equalTo(self.view.center)
+            make.width.equalTo(150)
+            make.height.equalTo(36)
+        }
+        
+        applyFilterButton.addTarget(self, action: #selector(apply), for: .touchUpInside)
+        
     }
 
+    @objc func addTapped() {
+        print("TAP")
+    }
+    
+    @objc func apply() {
+        print("apply")
+    }
 
 }
 
